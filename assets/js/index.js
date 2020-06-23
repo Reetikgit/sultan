a85681920e81e88535adeda400369cea1f2dfdef
bhagatsingh1998 <bhagatsingh.17is@saividya.ac.in>
2020-06-23 22:02:21 +0530

    changes on exsisting pages

---
+++ assets/js/index.js
@@ -0,0 +1,7 @@
+
+document.querySelector('.collapsible').addEventListener('click', function(e) {
+  // document.querySelector('.showFilter').style.display = 'visible';
+  // document.querySelector('.filter-grid').style.display = 'visible';
+  document.querySelector('.showFilter').classList.toggle('showFilter');
+
+})
